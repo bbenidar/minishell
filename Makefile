5ae09c60f0c915ba29a6de3570646c8ef54a3acd
Saad Karkal <karkalsaad@gmail.com>
2023-07-31 07:24:06 +0100

    .

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: sakarkal <sakarkal@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/30 14:11:37 by bbenidar          #+#    #+#              #
-#    Updated: 2023/07/30 01:34:18 by sakarkal         ###   ########.fr        #
+#    Updated: 2023/07/31 07:23:05 by sakarkal         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,7 +19,8 @@ SRC =  ./parsing/first_check.c ./parsing/check_utils.c minishell.c \
 		./lexical/lexical_func.c  ./lexical/linked_list_creation.c ./utils/utils1.c ./utils/utils2.c ./utils/utils3.c \
 		./parsing/splite_utile.c  ./utils/utils4.c ./utils/itoa.c ./execution/execut.c  ./execution/builting/echo.c\
 		 ./execution/builting/cd.c ./execution/builting/env.c execution/builting/export.c execution/builting/pwd.c\
-		execution/builting/unset.c	utils/atoll.c execution/builting/exit.c
+		execution/builting/unset.c	utils/atoll.c execution/builting/exit.c execution/builting/export_util.c \
+		utils/utils.c 
 OBG = ${SRC:.c=.o}
 
 HEAD="\n"\
--- minishell.c
+++ minishell.c
@@ -3,29 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbenidar <bbenidar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sakarkal <sakarkal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 17:17:03 by bbenidar          #+#    #+#             */
-/*   Updated: 2023/07/31 00:04:55 by bbenidar         ###   ########.fr       */
+/*   Updated: 2023/07/31 07:22:44 by sakarkal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_envir *creat_env_list()
+t_envir	*creat_env_list(void)
 {
-	t_envir *list;
+	t_envir	*list;
 
 	list = malloc(sizeof(t_envir) * 1);
 	list->next = NULL;
 	return (list);
 }
 
-char *ft_variabl(char *str)
+char	*ft_variabl(char *str)
 {
-	int i = 0;
-	char *ret;
+	int		i;
+	char	*ret;
 
+	i = 0;
 	while (str[i] != '=')
 		i++;
 	ret = malloc(sizeof(char) * (i + 1));
@@ -38,11 +39,12 @@ char *ft_variabl(char *str)
 	return (ret);
 }
 
-char *ft_value(char *str)
+char	*ft_value(char *str)
 {
-	int i = 0;
-	char *ret;
+	int		i;
+	char	*ret;
 
+	i = 0;
 	while (*str && *str != '=')
 		str++;
 	str++;
@@ -56,13 +58,13 @@ char *ft_value(char *str)
 	return (ret);
 }
 
-t_envir *ft_no_env()
+t_envir	*ft_no_env(void)
 {
-	char buffer[4096];
+	char	buffer[4096];
+	t_envir	*list;
+	t_envir	*ret;
+
 	getcwd(buffer, sizeof(buffer));
-	t_envir *list;
-	t_envir *ret;
-	// flags.shlvl = ft_get_shlvl();
 	list = creat_env_list();
 	ret = list;
 	list->variable = ft_strdup("PWD");
@@ -84,17 +86,14 @@ t_envir *ft_no_env()
 	list->equal = ft_strdup("=");
 	list->value = ft_strdup("/usr/gnu/bin:/usr/local/bin:/bin:/usr/bin:.");
 	flags.path_fl = 1;
-	
-
-	return(ret);
-	
+	return (ret);
 }
 
-t_envir *replace_variables(char **env)
+t_envir	*replace_variables(char **env)
 {
-	t_envir *list;
-	t_envir *ret;
-	int i;
+	t_envir	*list;
+	t_envir	*ret;
+	int		i;
 
 	i = 0;
 	if (env[0])
@@ -119,10 +118,7 @@ t_envir *replace_variables(char **env)
 	return (ret);
 }
 
-
-
-
-void ft_sigint(int sig)
+void	ft_sigint(int sig)
 {
 	(void)sig;
 	write(1, "\n", 1);
@@ -131,12 +127,11 @@ void ft_sigint(int sig)
 	rl_redisplay();
 }
 
-void begin(char **env)
+void	begin(char **env)
 {
-
-	t_envir *envr;
-	t_envir *tmp;
-	char *line;
+	t_envir	*envr;
+	t_envir	*tmp;
+	char	*line;
 
 	signal(SIGINT, ft_sigint);
 	envr = replace_variables(env);
@@ -148,32 +143,26 @@ void begin(char **env)
 		if (!line)
 		{
 			printf("exit\n");
-			break;
+			break ;
 		}
 		if (!check_space(line))
 			add_history(line);
-		
-		
 		if (!ft_first_check(line))
 		{
 			line = ft_add_variables(line, envr);
-			printf("nb %d \n", flags.delim_flags);
-
 			lexical_function(line, env, envr);
 		}
-		else 
+		else
 			flags.exit_stat = 66048;
 		free(line);
 	}
 }
 
-#include <string.h>
-int main(int ac, char **av, char **env)
+int	main(int ac, char **av, char **env)
 {
 	av[1] = 0;
 	if (ac != 1)
 		return (0);
-	
 	// rl_catch_signals = 0;
 	printf("\033[2J\033[1;1H");
 	printf("\n");
